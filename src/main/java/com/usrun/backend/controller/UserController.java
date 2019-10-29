00d31cc5602f80c93ff2bce3415181c97205df95
tranphucbol <tranphucbol@gmail.com>
2019-10-29 13:00:33 +0700

    done user/filter

--- src/main/java/com/usrun/backend/controller/UserController.java
+++ src/main/java/com/usrun/backend/controller/UserController.java
@@ -2,17 +2,25 @@ package com.usrun.backend.controller;
 
 import com.usrun.backend.exception.ResourceNotFoundException;
 import com.usrun.backend.model.User;
+import com.usrun.backend.payload.CodeResponse;
 import com.usrun.backend.payload.UserInfoResponse;
 import com.usrun.backend.repository.UserRepository;
 import com.usrun.backend.security.CurrentUser;
 import com.usrun.backend.security.TokenProvider;
 import com.usrun.backend.security.UserPrincipal;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.constraints.Min;
+import java.util.List;
 
 @RestController
 public class UserController {
@@ -31,5 +39,16 @@ public class UserController {
         String jwt = tokenProvider.createTokenUserId(user.getId());
 
         return new ResponseEntity<>(new UserInfoResponse(user, jwt), HttpStatus.OK);
+    }
+
+    @GetMapping("/user/filter")
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<?> findUser(
+            @RequestParam(name = "key", required = false) String key,
+            @Min(0) @RequestParam(name = "offset", defaultValue = "0") Integer offset,
+            @Min(1) @RequestParam(name = "count", defaultValue = "30") Integer count) {
+        Pageable pageable = PageRequest.of(offset, count);
+        List<User> users = userRepository.findUser('%' + key + '%', pageable);
+        return new ResponseEntity<>(new CodeResponse(users), HttpStatus.OK);
     }
 }
--- src/main/java/com/usrun/backend/payload/CodeResponse.java
+++ src/main/java/com/usrun/backend/payload/CodeResponse.java
@@ -1,5 +1,6 @@
 package com.usrun.backend.payload;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,7 +9,15 @@ import lombok.Setter;
 public class CodeResponse {
     private int code;
 
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Object data;
+
     public CodeResponse(int code) {
         this.code = code;
+    }
+
+    public CodeResponse(Object data) {
+        this.code = 0;
+        this.data = data;
     }
 }
