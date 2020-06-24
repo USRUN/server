30cdeb2821b75e12f54bc4a6812e19c4fd9d6bea
phuctt4 <phuctt4@vng.com.vn>
2020-06-25 00:29:53 +0700

    transfer

--- src/main/java/com/usrun/core/exception/CodeException.java
+++ src/main/java/com/usrun/core/exception/CodeException.java
@@ -1,20 +1,32 @@
 package com.usrun.core.exception;
 
+import com.usrun.core.config.ErrorCode;
+
 public class CodeException extends RuntimeException {
 
-  private int errorCode;
+  private ErrorCode errorCode;
 
   public CodeException(int errorCode) {
     super(Integer.toString(errorCode));
-    this.errorCode = errorCode;
+    this.errorCode = ErrorCode.fromInt(errorCode);
   }
 
   public CodeException(String message, int errorCode) {
     super(message);
+    this.errorCode = ErrorCode.fromInt(errorCode);
+  }
+
+  public CodeException(ErrorCode errorCode) {
+    super(errorCode.name());
     this.errorCode = errorCode;
   }
 
-  public int getErrorCode() {
+  public CodeException(String message, ErrorCode errorCode) {
+    super(message);
+    this.errorCode = errorCode;
+  }
+
+  public ErrorCode getErrorCode() {
     return errorCode;
   }
 }
