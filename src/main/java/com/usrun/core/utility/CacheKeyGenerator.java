a94231da1b4c8af5d5c5bc0e5026e4dd3e3b456e
phuctt4 <phuctt4@vng.com.vn>
2020-08-23 19:22:46 +0700

    add env to key of redis

--- src/main/java/com/usrun/core/utility/CacheKeyGenerator.java
+++ src/main/java/com/usrun/core/utility/CacheKeyGenerator.java
@@ -1,5 +1,7 @@
 package com.usrun.core.utility;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Component;
 
 /**
@@ -9,51 +11,57 @@ import org.springframework.stereotype.Component;
 @Component
 public class CacheKeyGenerator {
 
+  private final String PREFIX;
+
+  public CacheKeyGenerator(@Value("${spring.profiles.active}") String env) {
+    this.PREFIX = env + ":";
+  }
+
   public String keyVerifyOtp(long userId) {
-    return "users:otp:" + userId;
+    return PREFIX + "users:otp:" + userId;
   }
 
   public String keyTrack(long trackId) {
-    return "track:" + trackId;
+    return PREFIX + "track:" + trackId;
   }
 
   public String keyUser(long userId) {
-    return "user:" + userId;
+    return PREFIX + "user:" + userId;
   }
 
   public String keyUserEmail(String email) {
-    return "users:email:" + email;
+    return PREFIX + "users:email:" + email;
   }
 
   public String keyTrackSig(long trackId, String sig) {
-    return "track:sig:" + trackId + sig;
+    return PREFIX + "track:sig:" + trackId + sig;
   }
 
   public String keyTeamMemberType(long teamId, long userId) {
-    return "team:role:" + teamId + ":" + userId;
+    return PREFIX + "team:role:" + teamId + ":" + userId;
   }
 
   public String keyActivity(long activityId) {
-    return "activity:" + activityId;
+    return PREFIX + "activity:" + activityId;
   }
 
   public String keyActivitySortedSet(long teamId) {
-    return "activities:team:" + teamId;
+    return PREFIX + "activities:team:" + teamId;
   }
 
   public String keyActivityCountByTeam(long teamId) {
-    return "activities:team:count:" + teamId;
+    return PREFIX + "activities:team:count:" + teamId;
   }
 
   public String keyActivityLock(long userId, long time) {
-    return "activity:lock:" + userId + ":" + time;
+    return PREFIX + "activity:lock:" + userId + ":" + time;
   }
   
   public String keyTeamStat(){
-      return"team:stat:";
+      return PREFIX + "team:stat:";
   }
 
   public String keyLoveCount(long activityId) {
-    return "activity:love:" + activityId;
+    return PREFIX + "activity:love:" + activityId;
   }
 }
