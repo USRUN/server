d1ad61ee60c5ffbaf0d308ac7e4c58305d7099fd
HuyNa <anhhuy980413@gmail.com>
2020-08-26 21:19:37 +0700

    update

--- src/main/java/com/usrun/core/service/GoogleMapService.java
+++ src/main/java/com/usrun/core/service/GoogleMapService.java
@@ -57,13 +57,15 @@ public class GoogleMapService {
         mergeData.stream().forEach(item -> params.append("|").append(item.getLatitude()).append(",").append(item.getLongitude()));
         String query = URLEncoder.encode(params.toString(), StandardCharsets.UTF_8.name());
         String key = appProperties.getGoogleMapKey();
-        String marker ="";
+        String markerStart = "";
+        String markerFinish = "";
         if (!mergeData.isEmpty()) {
-            marker = "&markers=color:blue|label:S|" + mergeData.get(0).getLatitude() + "," + mergeData.get(0).getLongitude()
-                    + "&markers=color:blue|label:F|" + mergeData.get(mergeData.size() - 1).getLatitude() + "," + mergeData.get(mergeData.size() - 1).getLongitude();
+            markerStart = "color:blue|label:S|" + mergeData.get(0).getLatitude() + "," + mergeData.get(0).getLongitude();
+            markerFinish = "color:blue|label:F|" + mergeData.get(mergeData.size() - 1).getLatitude() + "," + mergeData.get(mergeData.size() - 1).getLongitude();
 
         }
-        query = "size=720x720&path=" + query +URLEncoder.encode(marker, StandardCharsets.UTF_8.name())+ "&key=" + key;
+        query = "size=720x720&path=" + query +"&markers="+ URLEncoder.encode(markerStart, StandardCharsets.UTF_8.name()) +"&markers="+
+                URLEncoder.encode(markerFinish, StandardCharsets.UTF_8.name()) + "&key=" + key;
         BufferedImage resultImage = ImageIO.read(new URL(url + query));
         String fileName = "activity-track-" + UUID.randomUUID().toString() + "." + "png";
         String fileUrl = amazonClient.uploadFile(resultImage, fileName, "png");
